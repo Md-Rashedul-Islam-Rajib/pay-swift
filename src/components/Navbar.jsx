bd630cb1038ee693d5e5acff7ac6ca07ce759d42
rajib5570 <rajib5570@gmail.com>
2024-07-16 03:09:00 +0600

    send money path added in the router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo-nav.png'
-import { GiPayMoney, GiTakeMyMoney } from 'react-icons/gi';
+import { GiCash, GiPayMoney, GiReceiveMoney, GiTakeMyMoney } from 'react-icons/gi';
+import { FaMoneyBillTransfer } from 'react-icons/fa6';
 const Navbar = () => {
     return (
         <nav className='hidden md:grid text-white space-y-6 text-center'>
@@ -10,9 +11,9 @@ const Navbar = () => {
             </div>
             <NavLink to='/send-money' ><p className='flex items-center gap-2 justify-center'><GiTakeMyMoney /> <span> Send Money </span></p></NavLink>
             <NavLink to='/cash-out' ><p className='flex items-center gap-2 justify-center'><GiPayMoney /> <span> Cash Out </span></p></NavLink>
-            <NavLink to='/cash-in' ><p className='flex items-center gap-2 justify-center'><GiTakeMyMoney /> <span> Cash In </span></p></NavLink>
-            <NavLink to='/balance-inquiry'><p className='flex items-center gap-2 justify-center'><GiTakeMyMoney /> <span> Balance Inquiry </span></p></NavLink>
-            <NavLink to='/transaction-history' ><p className='flex items-center gap-2 justify-center'><GiTakeMyMoney /> <span> Transaction History </span></p></NavLink>
+            <NavLink to='/cash-in' ><p className='flex items-center gap-2 justify-center'><GiReceiveMoney /> <span> Cash In </span></p></NavLink>
+            <NavLink to='/balance-inquiry'><p className='flex items-center gap-2 justify-center'><GiCash /> <span> Balance Inquiry </span></p></NavLink>
+            <NavLink to='/transaction-history' ><p className='flex items-center gap-2 justify-center'><FaMoneyBillTransfer /> <span> Transaction History </span></p></NavLink>
         </nav>
     );
 };
--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Mainlayout from "../layouts/Mainlayout";
 import Home from "../pages/Home";
+import SendMoney from "../pages/SendMoney";
 
 
 export const router = createBrowserRouter([
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
             path: '/',
             element : <Home />
         },
+        {
+            path : '/send-money',
+            element : <SendMoney />
+        }
 
     ]
     }
