048eecb2b52c6288e8f7bc3573140cddf1b35195
rajib5570 <rajib5570@gmail.com>
2024-07-16 02:44:57 +0600

    react icon added in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo-nav.png'
+import { GiPayMoney, GiTakeMyMoney } from 'react-icons/gi';
 const Navbar = () => {
     return (
         <nav className='hidden md:grid text-white space-y-6 text-center'>
             <div>
                 <img src={logo} alt="" />
             </div>
-            <NavLink to='/send-money' ><p>Send Money</p></NavLink>
-            <NavLink to='/cash-out' ><p>Cash Out</p></NavLink>
-            <NavLink to='/cash-in' ><p>Cash In</p></NavLink>
-            <NavLink to='/balance-inquiry'><p>Balance Inquiry</p></NavLink>
-            <NavLink to='/transaction-history' ><p>Transaction History</p></NavLink>
+            <NavLink to='/send-money' ><p className='flex items-center gap-2 justify-center'><GiTakeMyMoney /> <span> Send Money </span></p></NavLink>
+            <NavLink to='/cash-out' ><p className='flex items-center gap-2 justify-center'><GiPayMoney /> <span> Cash Out </span></p></NavLink>
+            <NavLink to='/cash-in' ><p className='flex items-center gap-2 justify-center'><GiTakeMyMoney /> <span> Cash In </span></p></NavLink>
+            <NavLink to='/balance-inquiry'><p className='flex items-center gap-2 justify-center'><GiTakeMyMoney /> <span> Balance Inquiry </span></p></NavLink>
+            <NavLink to='/transaction-history' ><p className='flex items-center gap-2 justify-center'><GiTakeMyMoney /> <span> Transaction History </span></p></NavLink>
         </nav>
     );
 };
