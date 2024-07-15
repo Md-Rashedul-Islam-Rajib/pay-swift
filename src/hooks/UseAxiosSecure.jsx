6e5caa3d91e1f4ae3eb73574ad2cbe612a290103
rajib5570 <rajib5570@gmail.com>
2024-07-16 03:39:09 +0600

    useAxiosSecure hook modified

---
+++ src/hooks/UseAxiosSecure.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
+
+
+export const axiosSecure = axios.create({
+    baseURL: 'https://assignment-12-server-gamma-sable.vercel.app'
+})
+
+const useAxiosSecure = () => {
+
+    const navigate = useNavigate();
+    const {logOutUser} = useContext(AuthContext);
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access-token');
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, 
+    function (error){
+        return Promise.reject(error);
+    });
+
+axiosSecure.interceptors.response.use((response)=> {
+    return response;
+},
+async (error)=> {
+    const status = error.response.status;
+    if(status == 401 || status == 403){
+      await logOutUser();
+      navigate('/login') 
+    }
+    return Promise.reject(error);
+}
+)
+    return axiosSecure;
+};
+
+export default useAxiosSecure;
