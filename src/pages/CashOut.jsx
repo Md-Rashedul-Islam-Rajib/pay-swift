4938f6fff8340dddd129b2e377a483343edfca03
rajib5570 <rajib5570@gmail.com>
2024-07-16 20:15:23 +0600

    cashout page modified

--- src/pages/CashOut.jsx
+++ src/pages/CashOut.jsx
@@ -15,8 +15,8 @@ const CashOut = () => {
 
     return (
         <section className="grid grid-cols-1 md:grid-cols-2 items-center h-screen justify-center bg-[#ff9a3c]">
-      <div className="flex-col items-center text-white md:ml-[20%] space-y-12">
-        <h1 className="font-bold text-xl md:text-6xl">Lowest Cash <br /> Out Fee</h1>
+      <div className="flex-col items-center text-center md:text-start text-white md:ml-[20%] md:space-y-12">
+        <h1 className="font-bold text-3xl md:text-6xl">Lowest Cash <br /> Out Fee</h1>
         <p className="font-semibold  md:text-xl">Easy Cash Out With Only 1.5% Fee</p>
       </div>
       <div>
