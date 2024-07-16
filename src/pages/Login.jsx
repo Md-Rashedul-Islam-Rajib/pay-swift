2c21b20cd0bbc539226a5b36ab732b03d99cb407
rajib5570 <rajib5570@gmail.com>
2024-07-16 23:29:43 +0600

    login page modified

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Login = () => {
+    
+    const CashIn = () => {
+        const {
+          register,
+          handleSubmit,
+          formState: { errors },
+        } = useForm();
+
     return (
         <div>
-            login
+            
         </div>
     );
 };
