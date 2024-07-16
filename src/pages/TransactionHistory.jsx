e88ab9708a79aa6046060afd251903f0ae05f150
rajib5570 <rajib5570@gmail.com>
2024-07-16 21:14:25 +0600

    transaction history page modified

--- src/pages/TransactionHistory.jsx
+++ src/pages/TransactionHistory.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const TransactionHistory = () => {
     return (
-        <div>
+        <section>
+            <div>
+            <h1 className="font-bold text-2xl md:text-5xl text-center">Transaction History</h1>
+            </div>
             
-        </div>
+        </section>
     );
 };
 
