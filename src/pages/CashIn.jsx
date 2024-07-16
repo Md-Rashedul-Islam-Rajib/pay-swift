70cf5e58f3df3b74bfe17d8fa5a16959d5d0042c
rajib5570 <rajib5570@gmail.com>
2024-07-16 20:41:17 +0600

    cashin page modified

--- src/pages/CashIn.jsx
+++ src/pages/CashIn.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-
 const CashIn = () => {
   const {
     register,
@@ -13,61 +12,74 @@ const CashIn = () => {
     console.log(data);
   };
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 items-center h-screen justify-center bg-[#ff7e67]">
-      <div className="flex-col items-center text-center md:text-start text-white md:ml-[20%] md:space-y-12">
+    <section className="grid grid-cols-1 md:grid-cols-2 md:items-center h-screen justify-center bg-[#ff7e67]">
+      <div className="flex-col items-center pt-[30%] md:pt-0 text-center md:text-start text-white md:ml-[20%] space-y-4 md:space-y-12">
         <h1 className="font-bold text-3xl md:text-6xl">Free Cash In</h1>
-        <p className="font-semibold  md:text-xl">Enjoy Unlimited Cash In Without Any Fee</p>
+        <p className="font-semibold  md:text-xl">
+          Enjoy Unlimited Cash In Without Any Fee
+        </p>
       </div>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="max-w-[75%] mx-auto space-y-10">
+            <div>
+              <label className="input flex items-center gap-2 mb-2 md:mb-4">
+                <input
+                  type="number"
+                  className="grow py-2 px-6 rounded-lg"
+                  placeholder="Recipient Mobile No"
+                  {...register("phone", { required: true })}
+                />
+                {errors.phone && (
+                  <span className="text-red-600 text-sm">
+                    Agent number is required
+                  </span>
+                )}
+              </label>
+            </div>
 
-          <div className="max-w-[75%] mx-auto">
-          <label className="input flex items-center gap-2 mb-2 md:mb-4">
-            <input
-              type="number"
-              className="grow py-2 px-6 rounded-lg"
-              placeholder="Recipient Mobile No"
-              {...register("phone", { required: true })}
-            />
-            {errors.phone && (
-              <span className="text-red-600 text-sm">Agent number is required</span>
-            )}
-          </label>
-
-          <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
-            <input
-            //   type={showPassword ? "text" : "password"}
-              className="grow py-2 px-6 rounded-lg"
-              type="number"
-              placeholder="Amount"
-              {...register("amount", { required: true })}
-            />
-            {/* <span
+            <div>
+              <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
+                <input
+                  //   type={showPassword ? "text" : "password"}
+                  className="grow py-2 px-6 rounded-lg"
+                  type="number"
+                  placeholder="Amount"
+                  {...register("amount", { required: true })}
+                />
+                {/* <span
               onClick={() => {
                 setShowPassword(!showPassword);
               }}
             >
               {showPassword ? <FaEyeSlash /> : <FaEye />}
             </span> */}
-            {errors.amount && (
-              <span className="text-red-600 text-sm">Amount is required</span>
-            )}
-          </label>
+                {errors.amount && (
+                  <span className="text-red-600 text-sm">
+                    Amount is required
+                  </span>
+                )}
+              </label>
+            </div>
 
-          <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
-            <input
-              type="number"
-              className="grow py-2 px-6 rounded-lg"
-              placeholder="Pin"
-              {...register("pin", { required: true })}
-            />
-            {errors.pin && (
-              <span className="text-red-600 text-sm">Pin is required</span>
-            )}
-          </label>
+            <div>
+              <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
+                <input
+                  type="number"
+                  className="grow py-2 px-6 rounded-lg"
+                  placeholder="Pin"
+                  {...register("pin", { required: true })}
+                />
+                {errors.pin && (
+                  <span className="text-red-600 text-sm">Pin is required</span>
+                )}
+              </label>
+            </div>
 
             <div className="flex justify-center">
-          <button className="btn bg-[#155263] text-white py-2 px-4 rounded-lg">Confirm</button>
+              <button className="btn bg-[#155263] text-white py-2 px-4 rounded-lg">
+                Confirm
+              </button>
             </div>
           </div>
         </form>
